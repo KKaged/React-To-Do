4f9c209da75333a2f0dcfd5210dd1e97404d1b74
KKaged <dhigaredaleon707@gmail.com>
2024-02-06 21:30:45 +0900

    finished project, can delete, add

--- react-to-do/src/app/components/List.jsx
+++ react-to-do/src/app/components/List.jsx
@@ -15,6 +15,10 @@ export default function ListTable() {
     });
     setTasks(newTasks);
   }
+  function deleteTask(index) {
+    const newTasks = tasks.filter((task, i) => i !== index);
+    setTasks(newTasks);
+  }
 
   return (
     <>
@@ -41,31 +45,43 @@ export default function ListTable() {
                 value={taskName}
                 onChange={(e) => setTaskName(e.target.value)}
               />
-              <button
-                className="border-2 font-light text-white text-lg rounded border-lime-900 bg-lime-700 hover:bg-lime-800"
-                onClick={() => {
-                  setTasks([...tasks, { name: taskName, isFinished: false }]);
-                  setTaskName("");
-                  setEditing(!editing);
-                }}
-              >
-                Add
-              </button>
+              <div className="flex gap-2">
+                <button
+                  className="border-2 font-light text-white text-lg rounded border-lime-900 bg-lime-700 hover:bg-lime-800"
+                  onClick={() => {
+                    setEditing(!editing);
+                    setTaskName("");
+                  }}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="border-2 font-light text-white text-lg rounded border-lime-900 bg-lime-700 hover:bg-lime-800"
+                  onClick={() => {
+                    setTasks([...tasks, { name: taskName, isFinished: false }]);
+                    setTaskName("");
+                    setEditing(!editing);
+                  }}
+                >
+                  Add
+                </button>
+              </div>
             </>
           )}
         </div>
-        <table className="w-full bg-white rounded-lg">
+        <table className="w-full bg-white rounded-lg ">
           <thead>
             <tr>
-              <th className="text-left">Task</th>
+              <th className="text-center text-lg font-medium">Task</th>
             </tr>
           </thead>
           <tbody>
             {tasks.map((task, index) => (
               <tr key={index}>
-                <td className=" flex text-left font-light gap-5">
+                <td className=" flex text-left font-light gap-5 text-xl mx-3">
                   <input
-                    type="checkbox" // Use checkbox for better UX
+                    type="checkbox"
+                    className="accent-green-900 "
                     checked={task.isFinished}
                     onChange={() => toggleFinished(index)}
                   />
@@ -76,6 +92,25 @@ export default function ListTable() {
                   >
                     {task.name}
                   </span>
+                  <button
+                    className="text-red-700 font-light text-xl hover:animate-pulse"
+                    onClick={() => deleteTask(index)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </button>
                 </td>
               </tr>
             ))}
